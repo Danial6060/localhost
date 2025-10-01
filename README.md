f0b21f757f3b8e456f1df8fe8a89f5c7a763fb2e
dalajmi <danial6060@gmail.com>
2025-10-01 22:59:51 +0300

    lol

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+Run these commands to start server:
+
+export CARGO_HOME="$HOME/.cargo"
 cargo build --release
 ./target/release/webserv config.conf
--- target/release/deps/webserv-991248d33b282b25.d
+++ target/release/deps/webserv-991248d33b282b25.d
@@ -1,6 +1,6 @@
-/home/dalajmi/localhost/target/release/deps/webserv-991248d33b282b25: src/main.rs src/config.rs src/epoll_handler.rs src/http_parser.rs src/http_response.rs src/server.rs src/cgi.rs src/session.rs
+/home/dalajmi/localhost-1/target/release/deps/webserv-991248d33b282b25: src/main.rs src/config.rs src/epoll_handler.rs src/http_parser.rs src/http_response.rs src/server.rs src/cgi.rs src/session.rs
 
-/home/dalajmi/localhost/target/release/deps/webserv-991248d33b282b25.d: src/main.rs src/config.rs src/epoll_handler.rs src/http_parser.rs src/http_response.rs src/server.rs src/cgi.rs src/session.rs
+/home/dalajmi/localhost-1/target/release/deps/webserv-991248d33b282b25.d: src/main.rs src/config.rs src/epoll_handler.rs src/http_parser.rs src/http_response.rs src/server.rs src/cgi.rs src/session.rs
 
 src/main.rs:
 src/config.rs:
